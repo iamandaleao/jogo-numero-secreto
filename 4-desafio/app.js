3a1b5b8f65dc8f768b902356e5df6de843c5fe30
iamandaleao <amandabibiano144@gmail.com>
2025-07-30 17:55:31 -0300

    feat: add class exercise

---
+++ 4-desafio/app.js
@@ -0,0 +1,24 @@
+// Cria uma lista vazia chamada listaGenerica
+let listaGenerica = [];
+
+// Cria uma lista com linguagens de programação
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Adiciona novos elementos ao final da lista linguagensDeProgramacao
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+// Resultado final da lista: ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby', 'GoLang']
+
+// Cria uma lista com 3 nomes
+let nomes1 = ['JavaScript', 'Python', 'Go'];
+// Exibe o primeiro elemento da lista (índice 0)
+console.log(nomes1[0]); // Saída: JavaScript
+
+// Cria outra lista com 3 nomes
+let nomes2 = ['JavaScript', 'Python', 'Go'];
+// Exibe o segundo elemento da lista (índice 1)
+console.log(nomes2[1]); // Saída: Python
+
+// Cria mais uma lista com 3 nomes
+let nomes3 = ['JavaScript', 'Python', 'Go'];
+// Exibe o último elemento da lista (índice 2)
+console.log(nomes3[2]); // Saída: Go
